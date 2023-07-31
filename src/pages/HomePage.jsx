e11df21a69d29580e0c9ec2ed0ac93c88b4d2dc5
madyanes <madyanes.dev@gmail.com>
2023-07-31 08:30:02 +0700

    create <SearchBar /> hanya UI, belum ada fungsionalitas

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ContactList from '../components/ContactLIst'
+import SearchBar from '../components/SearchBar'
 import { deleteContact, getContacts } from '../utils/data'
 
 class HomePage extends React.Component {
@@ -8,9 +9,11 @@ class HomePage extends React.Component {
 
         this.state = {
             contacts: getContacts(),
+            keyword: '',
         }
 
         this.onDeleteHandler = this.onDeleteHandler.bind(this)
+        this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this)
     }
 
     onDeleteHandler(id) {
@@ -24,9 +27,21 @@ class HomePage extends React.Component {
         })
     }
 
+    onKeywordChangeHandler(keyword) {
+        this.setState(() => {
+            return {
+                keyword,
+            }
+        })
+    }
+
     render() {
         return (
             <section>
+                <SearchBar
+                    keyword={this.state.keyword}
+                    keywordChange={this.onKeywordChangeHandler}
+                />
                 <h2>Daftar Kontak</h2>
                 <ContactList
                     contacts={this.state.contacts}
