e58909a65f4299d9f2e81e06e249abcba767cae1
madyanes <madyanes.dev@gmail.com>
2023-07-31 08:58:40 +0700

    integrating search bar 1. Mengintegrasikan search bar dengan query parameter 2. Filter kontak berdasarkan keyword (kata kunci)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 import ContactList from '../components/ContactLIst'
 import SearchBar from '../components/SearchBar'
 import { deleteContact, getContacts } from '../utils/data'
+
+const HomePageWrapper = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const keyword = searchParams.get('keyword')
+
+    const changeSearchParams = (keyword) => {
+        setSearchParams({ keyword })
+    }
+
+    return (
+        <HomePage
+            defaultKeyword={keyword}
+            keywordChange={changeSearchParams}
+        />
+    )
+}
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -9,7 +27,7 @@ class HomePage extends React.Component {
 
         this.state = {
             contacts: getContacts(),
-            keyword: '',
+            keyword: props.defaultKeyword || '',
         }
 
         this.onDeleteHandler = this.onDeleteHandler.bind(this)
@@ -33,9 +51,15 @@ class HomePage extends React.Component {
                 keyword,
             }
         })
+
+        this.props.keywordChange(keyword)
     }
 
     render() {
+        const contacts = this.state.contacts.filter((contact) => {
+            return contact.name.toLowerCase().includes(this.state.keyword.toLowerCase())
+        })
+
         return (
             <section>
                 <SearchBar
@@ -44,7 +68,7 @@ class HomePage extends React.Component {
                 />
                 <h2>Daftar Kontak</h2>
                 <ContactList
-                    contacts={this.state.contacts}
+                    contacts={contacts}
                     onDelete={this.onDeleteHandler}
                 />
             </section>
@@ -52,4 +76,4 @@ class HomePage extends React.Component {
     }
 }
 
-export default HomePage
+export default HomePageWrapper
