7373530215b2dbadd977234d634903c675b61fa4
madyanes <madyanes.dev@gmail.com>
2023-08-06 01:42:28 +0700

    bugfix: typo

--- src/contexts/LocaleContexts.jsx
+++ src/contexts/LocaleContexts.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 const LocaleContext = React.createContext()
 
 export const LocaleProvider = LocaleContext.Provider
-export const LocaleConsumer = LocaleContext.Consuemr
+export const LocaleConsumer = LocaleContext.Consumer
 
 export default LocaleContext
